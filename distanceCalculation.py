85f70cd5c698fda9cd38b1ecb95200282c798eb3
LucieChasseur <68419457+LucieChasseur@users.noreply.github.com>
2020-09-18 10:13:43 +0200

    Update distanceCalculation.py

--- distanceCalculation.py
+++ distanceCalculation.py
@@ -2,23 +2,24 @@
 # -*-coding:utf-8-*-
 
 '''
-Code créé pour calculer la distance entre les pictogrammes d'une application de CAA
+Code qui calcule la distance entre les pictogrammes d'une application de CAA
 Prend en compte la difficulté du mouvement et la difficulté de la sélection
 '''
 
-# Importation du module mathématiques
+# Importation du module mathématique
 import math
 import sys
 
 inputFile = "ProloquoBrut.csv"
 
 if len(sys.argv) == 2:
-    inputFile = sys.argv[1]
+	inputFile = sys.argv[1]
 elif len(sys.argv) > 2:
-    print("Error: Zero or One argument needed")
-    sys.exit(2)
+	print("Error: Zero or One argument needed")
+	sys.exit(2)
 
 print("input file is " + inputFile)
+
 # Fichier brut à traiter
 rawFile = open(inputFile, "r")
 
@@ -37,84 +38,95 @@ m = 1
 # Définition du poids du temps de sélection
 n = 1
 
+
 # Traitement du fichier source
 for lines in rawFile:
-    lines = lines.lower()
-    sentence = lines.strip()
-    col = sentence.split("\t")
+	lines = lines.lower()
+	sentence = lines.strip()
+	col = sentence.split("\t")
 
-    # On gère le problème des lines semi vides créées par les liens entre les répertoires
-    if len(col) > 4:
-        # Décraration des variables
-        words = col[0]
-        column = col[2]
-        line = col[1]
-        page = col[3]
-        iden = col[4]
+	# On gère le problème des lines semi vides créées par les liens entre les répertoires
+	if len(col) > 4:
+		# Décraration des variables
+		words = col[0]
+		column = col[2]
+		line = col[1]
+		page = col[3]
+		iden = col[4]
 
-        # Récupération des coordonnées x,y
-        x = int(line)
-        y = int(column)
+		# Récupération des coordonnées x,y
+		x = int(line)
+		y = int(column)
 
-        # On stocke les coordonnées l'ID et x,y dans une liste
-        disTab.append([iden, x, y, words, page])
+		# On stocke les coordonnées l'ID et x,y dans une liste
+		disTab.append([iden, x, y, words, page])
 
-    # On récupère les liens entre les répertoires
-    elif len(col) > 1:
-        headLink = col[0]
-        pointedLink = col[1]
+	# On récupère les liens entre les répertoires
+	elif len(col) > 1:
+		headLink = col[0]
+		pointedLink = col[1]
 
-        # On stocke les liens entre les répertoires
-        linksList.append([headLink, pointedLink])
+		# On stocke les liens entre les répertoires
+		linksList.append([headLink, pointedLink])
 
 # On parcours la liste créé plus haut
 for i in range(0, len(disTab)):
-    # On crée une variable qui prend comme valeur l'ID du mot d'indice i
-    refID = disTab[i][0]
-    # On crée une variable qui prend comme valeur le nom de la page actuelle
-    currentPage = disTab[i][4]
-    # On créé une variable qui prend comme valeur le mot d'indice i
-    mot = disTab[i][3]
-    x1 = disTab[i][1]
-    y1 = disTab[i][2]
+	# On crée une variable qui prend comme valeur l'ID du mot d'indice i
+	refID = disTab[i][0]
+	# On crée une variable qui prend comme valeur le nom de la page actuelle
+	currentPage = disTab[i][4]
+	# On créé une variable qui prend comme valeur le mot d'indice i
+	mot = disTab[i][3]
+	x1 = disTab[i][1]
+	y1 = disTab[i][2]
 
-    for j in range(i, len(disTab)):
-        # On créé une nouvelle variable qui prend comme valeur l'ID du mot d'indice j
-        ID = disTab[j][0]
-        # On vérifie que l'on est toujours sur la bonne page
-        currentPage2 = disTab[j][4]
-        x2 = disTab[j][1]
-        y2 = disTab[j][2]
+	for j in range(i, len(disTab)):
+		# On créé une nouvelle variable qui prend comme valeur l'ID du mot d'indice j
+		ID = disTab[j][0]
 
-        if currentPage2 == currentPage:
-            # Si les deux IDs sont différents on récupère les coordonnées x et y de chacun
-            if refID != ID:
-                mot2 = disTab[j][3]
+		# On vérifie que l'on est toujours sur la bonne page
+		currentPage2 = disTab[j][4]
+		x2 = disTab[j][1]
+		y2 = disTab[j][2]
 
-                # Calcul des distances Euclidiennes
-                squaredDistance = (x1 - x2) ** 2 + (y1 - y2) ** 2
-                pictoDistance = math.sqrt(squaredDistance)
+		if currentPage2 == currentPage:
+			# Si les deux IDs sont différents on récupère les coordonnées x et y de chacun
+			if refID != ID:
+				mot2 = disTab[j][3]
 
-                # Ecriture du résultat dans un fichier
-                bowsFile.write(mot + "\t" + refID + "\t" + ID + "\t" + str(pictoDistance * m + n) + "\n")
-                bowsFile.write(mot2 + "\t" + ID + "\t" + refID + "\t" + str(pictoDistance * m + n) + "\n")
+				# Calcul des distances Euclidiennes
+				squaredDistance = (x1 - x2) ** 2 + (y1 - y2) ** 2
+				pictoDistance = math.sqrt(squaredDistance)
 
-    # On écrit le lien entre le pictogramme et la page
-    if linkCount < len(linksList):
-        # Ecriture du résultat dans un fichier
-        bowsFile.write(
-            "Lien Picto Page" + "\t" + linksList[linkCount][0] + "\t" + linksList[linkCount][1] + "\t" + str(
-                n) + "\n")
-        linkCount += 1
+				#Si le mot de d'arrivée de l'arc est un répertoire C=(P1,P2)m
+				if "_r@" in ID :
+					#On écrit la fomule sans le n
+					bowsFile.write("Mot à Répertoire" + "\t" + refID + "\t" + ID + "\t" + str(pictoDistance * m) + "\n")
+				else :
+					#Si le pictogarmme départ et celui d'arrivée sont des mots C=(P1,P2)m+n
+					# On écrit la formule normale
+					bowsFile.write("Mot à Mot" + "\t" + refID + "\t" + ID + "\t" + str(pictoDistance * m + n) + "\n")
+					bowsFile.write("Mot à Mot" + "\t" + ID + "\t" + refID + "\t" + str(pictoDistance * m + n) + "\n")
 
-    # On écrit le lien entre la page et le pictogramme
-    # On calcule la disantce entre le lien de la page et des pictogrammes à partir du pictogramme en haut à gauche avec x=1 et y=1
-    squaredDistance3 = (1 - x1) ** 2 + (1 - y1) ** 2
-    pageToPicto = math.sqrt(squaredDistance3)
+	# On écrit le lien entre un pictogramme directeur (plus, retour, pagination, flèche retour et répertoires) et la page
+	if linkCount < len(linksList):
+		# Formule correspondnat uniquement à l'action de sélection donc C=n
+		bowsFile.write("Picto directeur à Page" + "\t" + linksList[linkCount][0] + "\t" + linksList[linkCount][1] + "\t" + str(n) + "\n")
+		linkCount += 1
 
-    # Ecriture du résultat dans un fichier
-    bowsFile.write(
-        "Lien Page Picto" + "\t" + currentPage + "\t" + disTab[i][0] + "\t" + str(pageToPicto * m + n) + "\n")
+	# On écrit le lien entre la page et le pictogramme 
+	# On calcule la disantce entre le lien de la page et des pictogrammes à partir du pictogramme en haut à gauche avec x=1 et y=1
+	squaredDistance3 = (1 - x1) ** 2 + (1 - y1) ** 2
+	pageToPicto = math.sqrt(squaredDistance3)
+
+	#Si le pictogramme d'arrivée de l'arc est un répetoire C=(P(1,1)P2)m
+	if "_r@" in disTab[i][0] :
+		#On calcule sans le n
+		bowsFile.write("Page à Répertoire" + "\t" + currentPage + "\t" + disTab[i][0] + "\t" + str(pageToPicto* m) + "\n")
+	else :
+		# Si le pictogramme de départ est une page et celui d'arrivée un mot C=(P(1,1)P2)m+n
+		bowsFile.write("Page à Mot" + "\t" + currentPage + "\t" + disTab[i][0] + "\t" + str(pageToPicto * m + n) + "\n")
+
 
 bowsFile.close()
 rawFile.close()
